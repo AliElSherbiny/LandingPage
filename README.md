3fd6deaf1e40641fa1ae7f7b3c77c44b193b8e8b
Ali elsherbiny <alielsherbiny142@gmail.com>
2021-04-21 22:33:35 +0200

    In HTML:
    Adding one more section using HTML
    Including JS file.
    
    In JS file
    Updating navbar element (sections 1 to 4)
    Making the sectins in navbar clickable
    updating styles for navbar elements
    
    In Readme.md
    Updating Project Info

--- README.md
+++ README.md
@@ -11,3 +11,12 @@ The starter project has some HTML and CSS styling to display a static version of
 To get started, open `js/app.js` and start building out the app's functionality
 
 For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+## Personal Information
+
+Name : Ali mohamed elsherbiny
+Project title : landing-page project
+Project description : This project requires you to build a multi-section landing page, with a dynamically updating navigational menu based on the amount of content that is added to the page.
+Technologies used to create the project: HTML, CSS, Javscript. 
+license : none
+version : 1.0
--- js/app.js
+++ js/app.js
@@ -34,7 +34,57 @@
 */
 
 // build the nav
+const sectionsList = document.querySelectorAll('.landing__container h2'); /*returns node list of 4 elemts*/ 
+const parentClass = document.querySelector('.navbar__menu'); //change parent class style
+/*mainpulating nav list style*/ 
+parentClass.style.backgroundColor =  "rgb(136,203,171)";
+parentClass.style.textAlign = "center";
 
+let navList = document.createElement('ul');
+parentClass.appendChild(navList) ; 
+navList.style.fontSize ="20pt";
+navList.style.fontStyle = "bold";
+navList.style.textAlign="center";
+//navList.style.border = "10px solid black";
+navList.style.alignItems = "center";
+
+for (const SectionElement of sectionsList) {
+    //create element unordered list
+    // create element list of class header
+    //text within the element = section element
+    let navElement = navList.appendChild( document.createElement('li'));
+    navElement.textContent= SectionElement.textContent;
+    navElement.innerHTML += "&nbsp;";
+    navElement.style.border = "10px none black";
+    navElement.style.display= "inline-block";
+    navElement.style.cursor= "pointer"; 
+    
+  }
+  
+  //const x= document.querySelector('.landing__container h2');
+  function Showup(evt)
+  { 
+    for (const SectionElement of sectionsList) 
+    {
+         if(evt.target.textContent.trim() === SectionElement.textContent.trim()) 
+        {
+            SectionElement.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+            //console.log(SectionElement.textContent.trim());
+            //console.log(evt.target.textContent.trim());
+            //console.log (SectionElement.textContent.trim() === evt.target.textContent.trim() );
+        }
+    }
+
+  }
+
+  navList.addEventListener('click',Showup);
+/*
+  parentClass.addEventListener('click',  event => {
+    x.scrollIntoView(true);
+  });
+  */
+
+  //add event listener on ul , ake li event target , the event is click and the function is make the section on top of page 
 
 // Add class 'active' to section when near top of viewport
 
